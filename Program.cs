05b1d8612df29a1a0a4d9e1f07b20a59300adbc6
Letícia Lauriano <114084854+leticialauriano@users.noreply.github.com>
2023-08-17 12:39:06 -0300

    Adicionando Projeto

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AntSuc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--Programa Antecessor e Sucessor--\n");
+
+            Console.Write("\nInforme Seu Número: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"\nO Antecessor do seu número é: {numero - 1}\nO Sucessor do seu número é: {numero + 1} ");
+
+            Console.WriteLine("\n---Pressione ENTER para encerrar---");
+            Console.ReadLine();
+        }
+    }
+}
